035dfe8b80155a988201d945ef59f0a94902ad95
Max Fierro <elmasse@gmail.com>
2018-04-29 17:11:59 -0300

    reduce bundle size

--- components/code.js
+++ components/code.js
@@ -4,7 +4,7 @@ import Highlight from './highlight'
 export default ({className = "", children}) => {
   const [, lang] = className.split('-')
   if (lang) {
-    return <Highlight languages={['javascript', 'json', 'markdown','bash', 'yaml', 'xml']} className={className}>{children.join('')}</Highlight>
+    return <Highlight className={className}>{children.join('')}</Highlight>
   }
 
   return <code className={className}>{children}</code>
--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 const { default: config } = require('nextein/config')
-const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin')
+const { ContextReplacementPlugin } = require('webpack')
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = config({
   webpack: (config) => {
     config.plugins.push(
+      // new BundleAnalyzerPlugin({
+      //   analyzerMode: 'static',
+      //   analyzerPort: 8888,
+      //   openAnalyzer: false,
+      //   generateStatsFile: true
+      // }),
       new ContextReplacementPlugin(
-        /highlight\.js\/lib\/languages$/,
-        new RegExp(`^./(${['javascript', 'json', 'markdown', 'bash', 'yaml', 'xml'].join('|')})$`)
-      )
+        /moment[\/\\]locale$/,
+        /en/
+      )      
     )
 
     return config
