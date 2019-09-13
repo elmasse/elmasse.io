4dcf1b9c583524f1ff6ba18d45fff601bed88ab4
Maximiliano Fierro <elmasse@gmail.com>
2019-09-13 16:38:00 -0300

    Use nextein plugin for tiles. Run npm audit .

--- next.config.js
+++ next.config.js
@@ -3,6 +3,28 @@ const { ContextReplacementPlugin } = require('webpack')
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 module.exports = withNextein({
+  nextein: {
+    plugins: [
+      {
+        name: 'nextein-plugin-markdown'
+      },
+      {
+        name: '@geut/nextein-plugin-title-chicago-style',
+        options: {          
+          special: [
+            'elmasse', 
+            'npm',
+            'cmd-plus',
+            'elmasse-bundle',
+            'Ext.ux.Cover',
+            'Ext.i18n.Bundle',
+            'GithubPages',
+            'ES7', 'ES6', 'JWT', 'ExtJS'
+          ]
+        }
+      }
+    ]
+  },
   webpack: (config) => {
     config.plugins.push(
       // new BundleAnalyzerPlugin({
