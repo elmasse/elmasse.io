651dfe4318873acb4611f8303c866e8b5ad04f5d
Max Fierro <elmasse@gmail.com>
2020-09-11 09:20:44 -0300

    Update config. Remove unused local plugin

--- next.config.js
+++ next.config.js
@@ -6,14 +6,10 @@ module.exports = withNextein({
       {
         name: 'nextein-plugin-markdown',
         options: {
-          position: true, // need this for reading-time plugin.
-          raw: false,
           rehype: ['rehype-slug', 'rehype-autolink-headings', '@mapbox/rehype-prism']
         }
       },
-      {
-        name: 'nextein-plugin-reading-time',
-      },
+      'nextein-plugin-reading-time',
       {
         name: '@geut/nextein-plugin-title-chicago-style',
         options: {
@@ -30,9 +26,6 @@ module.exports = withNextein({
             'iPad', 'iOS'
           ]
         }
-      },
-      {
-        name: './plugins/remove-position'
       }
     ]
   }
--- plugins/remove-position/index.js
+++
@@ -1,8 +0,0 @@
-const removePosition = require('unist-util-remove-position')
-
-module.exports.transform = (options = {}, posts) => {
-  return posts.map(post => { 
-    post.content = removePosition(post.content)
-    return post
-   })
-}
