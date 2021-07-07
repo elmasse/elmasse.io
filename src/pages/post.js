c48ef40f16ddcf7232e239b45a7b40a699e1f210
Max Fierro <elmasse@gmail.com>
2021-07-07 19:06:09 -0300

    Fix disclaimer in dark mode.

--- src/pages/post.js
+++ src/pages/post.js
@@ -23,7 +23,7 @@ export default withPost(({ post }) => {
         <Content className='max-w-5xl mx-auto px-6 lg:px-0 py-20 prose dark:prose-dark prose-lg lg:prose-xl' {...post} />
       </article>
       <div className='max-w-7xl mx-auto px-6 py-20'>
-        <p className='text-sm'>
+        <p className='text-sm text-gray-800 dark:text-gray-50'>
           Any viewpoints and opinions expressed in this article are my own and do not, 
           in any way, reflect those of my employer, my colleagues, or anyone else. I speak only for myself, not for them.
         </p> 
